3b55fdeb017a5f2af32d4badc7574adaf2a17d5d
Yoonj310 <opi2982@gmail.com>
2024-12-21 04:00:03 +0900

    basename 추가 #10

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -24,5 +24,5 @@ const router = createBrowserRouter([
 ]);
 
 export default function Router() {
-    return <RouterProvider router={router} />
+    return <RouterProvider router={router} basename={process.env.PUBLIC_URL}/>
 }
