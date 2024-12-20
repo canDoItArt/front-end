1d80c990bbd47e509bcb49b111fc8ea109854d01
Yoonj310 <opi2982@gmail.com>
2024-12-21 03:39:51 +0900

    base 주소 설정 #10

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/front-end/", // GitHub Pages의 프로젝트 경로
+});
