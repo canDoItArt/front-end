517417b8159217a3451b166b248ab8103e64371a
Yoonj310 <opi2982@gmail.com>
2025-05-05 18:42:53 +0900

    알림 아이콘 제거 #38

--- src/components/LogoHeader.jsx
+++ src/components/LogoHeader.jsx
@@ -1,20 +1,22 @@
 import { BsBell } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
-export default function LogoHeader() {                 
+export default function LogoHeader() {
 
     return (
-        <div className="fixed top-0 max-w-[480px] w-full z-10 flex items-center justify-between bg-white px-6 py-4">
+        <div className="fixed top-0 max-w-[480px] w-full z-10 flex items-center justify-start bg-white px-6 py-4">
             {/* 로고 영역 */}
-            <img
-                src="/front-end/logo_3.png"
-                alt="Welcome Logo"
-                className="w-28 h-auto"
-            />
-            {/* 뒤로 가기 버튼 */}
             <Link to='/home'>
-                <BsBell className="w-5 h-auto"/>
+                <img
+                    src="/front-end/logo_3.png"
+                    alt="Welcome Logo"
+                    className="w-28 h-auto"
+                />
             </Link>
+            {/* 뒤로 가기 버튼 */}
+            {/* <Link to='/home'>
+                <BsBell className="w-5 h-auto"/>
+            </Link> */}
         </div>
     );
 }
