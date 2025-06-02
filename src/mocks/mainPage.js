21a534221e31065913a0a300876481d4602d9be0
Yoonj310 <opi2982@gmail.com>
2025-06-02 22:04:24 +0900

    필수 값 미입력 시 버튼 비활성화 #63

--- src/mocks/mainPage.js
+++ src/mocks/mainPage.js
@@ -14,16 +14,16 @@ const mainPageMockData = [
                 checkedDates: ["2024-11-05", "2024-11-07", "2025-01-27"],
             },
             {
-                color: "#4091EE",
-                checkedDates: ["2024-11-05", "2024-11-06", "2024-11-08", "2025-01-27"],
+                color: "#B1D854",
+                checkedDates: ["2024-11-05", "2024-11-10", "2025-01-27"],
             },
             {
                 color: "#7BBFF9",
                 checkedDates: ["2024-11-05", "2024-11-10", "2025-01-27"],
             },
             {
-                color: "#B1D854",
-                checkedDates: ["2024-11-05", "2024-11-10", "2025-01-27"],
+                color: "#4091EE",
+                checkedDates: ["2024-11-05", "2024-11-06", "2024-11-08", "2025-01-27"],
             },
             {
                 color: "#8B80DD",
@@ -31,9 +31,11 @@ const mainPageMockData = [
             },
         ],
         subGoals: [
-            { id: 1, name: "몸 만들기", color: "#B1D854" },
+            { id: 1, name: "구위", color: "#EB4335" },
             { id: 2, name: "제구", color: "#F7D04D" },
-            { id: 3, name: "구위", color: "#EB4335" },
+            { id: 3, name: "몸 만들기", color: "#B1D854" },
+            { id: 4, name: "운", color: "#4091EE" },
+            { id: 5, name: "인성", color: "#8B80DD" },
         ],
     },
     {
@@ -44,25 +46,25 @@ const mainPageMockData = [
             lastWeek: 64,
             thisWeek: 72,
         },
-        comment: "꾸준함이 결국 승리한다.",
+        comment: "앞만 보고 가자, 내 인생에 뒤란 없다.",
         strict: [
             {
                 color: "#EB4335",
                 checkedDates: ["2024-11-02", "2024-11-04"],
             },
             {
-                color: "#4091EE",
-                checkedDates: ["2024-11-01", "2024-11-03"],
-            },
-            {
                 color: "#B1D854",
                 checkedDates: ["2024-11-02", "2024-11-06"],
             },
+            {
+                color: "#4091EE",
+                checkedDates: ["2024-11-01", "2024-11-03"],
+            },
         ],
         subGoals: [
-            { id: 1, name: "체력 연습", color: "#B1D854" },
-            { id: 2, name: "정확성", color: "#4091EE" },
-            { id: 3, name: "민첩성", color: "#F7D04D" },
+            { id: 1, name: "체력 연습", color: "#EB4335" },
+            { id: 2, name: "정확성", color: "#B1D854" },
+            { id: 3, name: "민첩성", color: "#4091EE" },
         ],
     },
     {
@@ -73,24 +75,24 @@ const mainPageMockData = [
             lastWeek: 55,
             thisWeek: 43,
         },
-        comment: "성공은 반복된 실패를 넘어선다.",
+        comment: "앞만 보고 가자, 내 인생에 뒤란 없다.",
         strict: [
             {
-                color: "#EB4335",
+                color: "#F7D04D",
                 checkedDates: ["2024-11-08", "2024-11-09"],
             },
             {
-                color: "#4091EE",
+                color: "#70CCB1",
                 checkedDates: ["2024-11-10", "2024-11-12"],
             },
             {
-                color: "#B1D854",
+                color: "#4091EE",
                 checkedDates: ["2024-11-11", "2024-11-13"],
             },
         ],
         subGoals: [
-            { id: 1, name: "작전 연습", color: "#B1D854" },
-            { id: 2, name: "집중력", color: "#EB4335" },
+            { id: 1, name: "작전 연습", color: "#F7D04D" },
+            { id: 2, name: "집중력", color: "#70CCB1" },
             { id: 3, name: "기술 연습", color: "#4091EE" },
         ],
     },
